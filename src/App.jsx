5a9922a7f232f108ed486cd990b2f4660e063c8d
kartik294 <kartikgeu@gmail.com>
2024-06-19 13:27:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username == "user" && password == "password") {
+      setError("");
+      setIsLoggedIn(true);
+    } else {
+      setError("Invalid username or password");
+    }
+  }
+ 
+  return (
+    <>
+      <div className='app'>
+        <h1>Login Page</h1>
+        {error && <p>{error}</p>}
+        {isLoggedIn 
+        ? <p>Welcome, {username}!</p> 
+        : (
+          <form onSubmit={handleSubmit} className='form'>
+            <label htmlFor="username">Username:</label>
+            <input  
+              type='text' 
+              id="username" 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password:</label>
+            <input  
+              type='password' 
+              id="password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type='submit'>Submit</button>
+          </form>
+        )}
+      </div>  
+    </>
+  )
+}
+
+export default App
